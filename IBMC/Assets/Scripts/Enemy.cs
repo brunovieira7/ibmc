cce4e5c730da6872678f55891c4e76a4c586806b
brunovieira7 <brunovieira7@github.com>
2017-01-09 18:14:20 -0200

    Enemy taking damage in healthbar

--- IBMC/Assets/Scripts/Enemy.cs
+++ IBMC/Assets/Scripts/Enemy.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour {
 
+	public Image healthBar;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -16,7 +18,10 @@ public class Enemy : MonoBehaviour {
 	}
 
 	public void takeDamage(int dmg) {
-		Healthbar healthbar = GetComponent<Healthbar> ();
-		healthbar.takeDamage (dmg);
+		RectTransform rect = healthBar.GetComponent<RectTransform> ();
+		Vector3 currScale = rect.localScale;
+		currScale.x -= 0.1f;
+
+		rect.localScale = currScale;
 	}
 }
